075aa6630e3c8d86a3d614058e041f6ec8b482ab
Orlando-MN <u17208037@utp.edu.pe>
2026-05-05 00:24:47 -0500

    feat: adaptar Angular para usar cityName y corregir listado de hoteles

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { DpDatePickerModule } from 'ng2-date-picker';
 import { HotelListing } from '../hotel-listing/hotel-listing';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
     DpDatePickerModule,
-    HotelListing
-    ],
+    HotelListing,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -15,4 +19,18 @@ export class Home {
   datePickerConfig = {
     format: 'YYYY-MM-DD',
   };
+
+  startDate: string = '2024-11-03';
+  endDate: string = '2024-11-07';
+  cityInput: string = 'Santiago'; // Lo que escribes (ej. Santiago, California)
+  showResults: boolean = false;
+
+  onSearch() {
+    if (this.cityInput.trim() === '') {
+      alert('Por favor ingresa una ciudad');
+      return;
+    }
+    this.showResults = true;
+    console.log('Buscando... City:' + this.cityInput + ' Fechas:' + this.startDate + ' a ' + this.endDate);
+  }
 }
--- src/app/components/pages/hotel-listing/hotel-listing.ts
+++ src/app/components/pages/hotel-listing/hotel-listing.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Hotels } from '../../../services/hotels';
+import { Hotel } from '../../../interface/hotel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hotel-listing',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hotel-listing.html',
   styleUrl: './hotel-listing.scss',
 })
-export class HotelListing {}
+export class HotelListing implements OnInit, OnChanges {
+  @Input() startDate!: string;
+  @Input() endDate!: string;
+  @Input() cityName!: string; // Ahora recibe el nombre (string)
+  hotels: Hotel[] = [];
+
+  constructor(private hotelsService: Hotels) {}
+
+  ngOnInit() {
+    if (this.startDate && this.endDate && this.cityName) {
+      this.loadHotels();
+    }
+  }
+
+  ngOnChanges() {
+    if (this.startDate && this.endDate && this.cityName) {
+      this.loadHotels();
+    }
+  }
+
+  private loadHotels() {
+    this.hotelsService.getHotels(this.startDate, this.endDate, this.cityName).subscribe({
+      next: (data) => {
+        this.hotels = data;
+        console.log('Hoteles cargados:', this.hotels);
+      },
+      error: (err) => {
+        console.error('Error al conectar con el microservicio:', err);
+      }
+    });
+  }
+}
